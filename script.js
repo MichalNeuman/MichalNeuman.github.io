2d865f01b30ab322b17f18373e1aba0454ba941a
Michał Neuman <michal.neuman@edu.uekat.pl>
2024-05-26 22:12:48 +0200

    rejstracja

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.nav-list');
 
@@ -5,3 +6,13 @@ menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 }
+const firebaseConfig = {
+    apiKey: "AIzaSyDG_DI51gJV40bY47YG86kUe_eV-3HgGsc",
+    authDomain: "lucrum-login.firebaseapp.com",
+    projectId: "lucrum-login",
+    storageBucket: "lucrum-login.appspot.com",
+    messagingSenderId: "561968143412",
+    appId: "1:561968143412:web:cc537c4524f0ec5495fe61"
+  };
+
+ 
