62d7994e1ad6d7eb23e798f42f4b1f68393e5e88
Michał Neuman <michal.neuman@edu.uekat.pl>
2024-06-25 09:14:52 +0200

    usuniecie logowania

--- script.js
+++ script.js
@@ -6,13 +6,6 @@ menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 }
-const firebaseConfig = {
-    apiKey: "AIzaSyDG_DI51gJV40bY47YG86kUe_eV-3HgGsc",
-    authDomain: "lucrum-login.firebaseapp.com",
-    projectId: "lucrum-login",
-    storageBucket: "lucrum-login.appspot.com",
-    messagingSenderId: "561968143412",
-    appId: "1:561968143412:web:cc537c4524f0ec5495fe61"
-  };
+
 
  
